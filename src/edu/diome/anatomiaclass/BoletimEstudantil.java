4c79af5ca309dfc0f9266e5037014eb4f7e29703
Alcidival Júnior Lemos <alcidival@live.com>
2025-09-08 16:03:54 -0300

    AULA SOBRE PACOTES
    Aula falando como criar os pacotes dentro do projeto

--- src/edu/diome/anatomiaclass/BoletimEstudantil.java
+++ src/edu/diome/anatomiaclass/BoletimEstudantil.java
@@ -1,3 +1,5 @@
+package edu.diome.anatomiaclass;
+
 public class BoletimEstudantil {
 
     public static void main(String[] args) {
--- src/edu/diome/anatomiaclass/MinhaClasse.java
+++ src/edu/diome/anatomiaclass/MinhaClasse.java
@@ -1,3 +1,5 @@
+package edu.diome.anatomiaclass;
+
 public class MinhaClasse {
 
     public static void main(String[] args) {
