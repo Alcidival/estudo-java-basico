9a5596921965ae8174753c3a29719d3f1094e9c7
Alcidival Júnior Lemos <alcidival@live.com>
2025-09-08 18:12:55 -0300

    TIPOS DE STRINGs
    Exemplificando os tipos de STRINGs existentes.

--- src/edu/diome/tipovariaveis/tipovariaveis.java
+++ src/edu/diome/tipovariaveis/tipovariaveis.java
@@ -4,13 +4,13 @@ public class tipovariaveis {
 
     public static void main(String[] args) {
         /**
-         * TIPOS DE VARIÁVEIS NUMÉRICOS
+         * TIPOS DE VARIÁVEIS PRIMITIVO
          * OBS.:
          * - SE 'INT' COMEÇAR COM '0', DEVE SER TIPO 'STRING';
          * - SE 'LONG' COMEÇAR COM '0', DEVE SER TIPO 'STRING';
          * - 'LONG' PRECISA SEMPRE FINALIZAR COM 'L' PARA INFORMAR QUE NÃO É 'INT;
          * - 'FLOAT' PRECISA SEMPRE FINALIZAR COM 'F' PARA CONFIRMAR QUE É 'FLOAT';
-         *
+         * - 'CHAR' É SÓ UM CARATECE;
          */
         byte idade = 123;
         short ano = 2025;
@@ -18,6 +18,8 @@ public class tipovariaveis {
         long cpf = 74173185289L;
         float pi = 3.14F;
         double salario = 1571.23;
+        char numero = 0;
+        boolean verdadeiro = true;
 
         /**
          * ESSA SEQUÊNCIA É UMA DEMOSTRAÇÃO DA TRANSFORMAÇÃO CASTING
@@ -35,7 +37,14 @@ public class tipovariaveis {
          */
         final float VALOR_DE_PI = 3.14F;
 
+        /**
+         * TIPOS DE STRINGs
+         * OBS.:
+         * - A "" É USADO PARA STRINGs
+         * - A '' É USADO PARA UM ÚNICO CARACTERE
+         */
+        String meuNome = "Alcidival";
+        char letra = 'A';
     }
-
 
 }
