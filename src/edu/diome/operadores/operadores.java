45c4463079ec2a10897cb2e28866b7e29233c9ce
Alcidival Júnior Lemos <alcidival@live.com>
2025-09-09 10:28:54 -0300

    OPERAÇÕES MATEM´TICAS E CONCATENAÇÃO
    Exemplificando como funciona as operações matemáticas e as concatenações de STRINGs com INT

---
+++ src/edu/diome/operadores/operadores.java
@@ -0,0 +1,36 @@
+package edu.diome.operadores;
+
+public class operadores {
+
+    public static void main(String[] args) {
+        /**
+         * OPERADORES ARITIMÉTICOS
+         * - MÓDULO SERVE PARA OBTER O RESTO DE UMA DIVISÃO INTEIRA;
+         */
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20 / 4);
+
+        System.out.println(resultado);
+
+        /**
+         * OPERADOR COM STRING
+         * - QUNANDO UTILIZADO O OPERADOR '+' COM STRING ELE
+         *   FAZ A CONCATENAÇÃO DOS TEXTOS.
+         *   - 'INT' SEGUIDO DE 'STRING' A CONCATENAÇÃO É DE STRING
+         */
+
+        String meuNome = "Alcidival " + "Júnior";
+        String concatenacao = "?";
+        concatenacao = 1+1+1+"1";
+        concatenacao = 1+"1"+1+1;
+        concatenacao = 1+"1"+1+"1";
+        concatenacao = "1"+1+1+1;
+        concatenacao = "1" + (1+1+1);
+        System.out.println(meuNome);
+    }
+
+}
